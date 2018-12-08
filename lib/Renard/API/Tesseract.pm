9fb15427e82c70eef511a5b35ad9663d286b6316
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-12-07 23:10:41 -0600

    Initial commit

---
+++ lib/Renard/API/Tesseract.pm
@@ -0,0 +1,11 @@
+package Renard::API::Tesseract;
+
+use strict;
+use warnings;
+
+1;
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-API-Tesseract/>
+
+=cut
